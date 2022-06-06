64ac34eebdafa01566ee951b36d95069f1ca9cab
achilles <cgrahlmann@signaturescience.com>
2022-06-06 12:01:28 -0500

    Added predict view

--- backend/server/apps/endpoints/urls.py
+++ backend/server/apps/endpoints/urls.py
@@ -1,4 +1,4 @@
-# backend/server/apps/endpoints/urls.py file
+'''
 from django.conf.urls import include
 from django.urls import path
 from rest_framework.routers import DefaultRouter
@@ -17,4 +17,29 @@ router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
 urlpatterns = [
   #  url(r"^api/v1/", include(router.urls)),
     path('api/v1/', include(router.urls), name="API router"),
+]
+
+'''
+
+
+# file backend/server/apps/endpoints/urls.py
+from django.urls import path
+from django.conf.urls import include
+from rest_framework.routers import DefaultRouter
+
+from apps.endpoints.views import EndpointViewSet
+from apps.endpoints.views import MLAlgorithmViewSet
+from apps.endpoints.views import MLRequestViewSet
+from apps.endpoints.views import PredictView # import PredictView
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r"endpoints", EndpointViewSet, basename="endpoints")
+router.register(r"mlalgorithms", MLAlgorithmViewSet, basename="mlalgorithms")
+router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
+
+urlpatterns = [
+    path("api/v1/", include(router.urls)),
+    # add predict url
+    path("api/v1/<str:endpoint_name>/predict", PredictView.as_view(), name="predict"
+    ),
 ]
